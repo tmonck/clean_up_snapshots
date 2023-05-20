8f6f57cae8720e1e49a49d54ac521bddb0fd50ad
tom <tom>
2023-05-20 11:23:09 -0400

    Add delete happy path unittest

--- tests/common.py
+++ tests/common.py
@@ -1,19 +1,23 @@
 """Common utils"""
 import json
+from http import HTTPStatus
 
-from homeassistant.core import HomeAssistant
-from pytest_homeassistant_custom_component.common import MockConfigEntry, load_fixture
+from pytest_homeassistant_custom_component.common import load_fixture
 from pytest_homeassistant_custom_component.test_util.aiohttp import AiohttpClientMocker
 
-import custom_components.clean_up_snapshots_service
 
-
-async def setup_supervisor_integration(aioclient_mock: AiohttpClientMocker):
+async def setup_supervisor_integration(
+    aioclient_mock: AiohttpClientMocker, backup_slugs_to_delete=None
+):
     """"""
+    if backup_slugs_to_delete is None:
+        backup_slugs_to_delete = []
     headers = {"authorization": "Bearer %s" % None}
     aioclient_mock.get(
         "http://supervisor/backups",
         json=json.loads(load_fixture("backups.json")),
         headers=headers,
     )
-    aioclient_mock.delete("http://supervisor/backups/id", json={}, headers=headers)
+    for backup_slug in backup_slugs_to_delete:
+        url = f"http://supervisor/backups/%s" % backup_slug
+        aioclient_mock.delete(url, json={"result": "ok"}, headers=headers)
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,11 +1,13 @@
 """Tests of __init__.py"""
 import asyncio
+import json
 import logging
 
 import aiohttp
 import pytest
 from homeassistant.components.hassio import HassioAPIError
 from homeassistant.core import HomeAssistant
+from pytest_homeassistant_custom_component.common import load_fixture
 from pytest_homeassistant_custom_component.test_util.aiohttp import AiohttpClientMocker
 from yarl import URL
 
@@ -99,26 +101,20 @@ async def test_async_get_snapshots_logs_timeout_error_and_raises(
             assert log_message in caplog.text
 
 
-# @pytest.mark.asyncio
-# async def test_async_get_snapshots_logs_timeout_error_and_raises(
-#     hass: HomeAssistant, aioclient_mock: AiohttpClientMocker, caplog
-# ):
-#     headers = {"authorization": "Bearer %s" % None}
-#     aioclient_mock.get(
-#         "http://supervisor/backups",
-#         exc=asyncio.TimeoutError,
-#         headers=headers,
-#     )
-#     thing = CleanUpSnapshots(hass, None)
-#     try:
-#         _ = await thing.async_get_snapshots()
-#     except HassioAPIError as err:
-#         assert err is not None
-#         assert "Client timeout on GET /backups" == str(err)
-#     assert aioclient_mock.call_count == 1
-#     (method, url, _, headers) = aioclient_mock.mock_calls[0]
-#     assert method == "GET"
-#     assert url == URL(f"%s%s" % (SUPERVISOR_URL, BACKUPS_URL_PATH))
-#     for record in caplog.records:
-#         if record.levelname == "ERROR":
-#             assert "timeout error on get snapshots" in caplog.text
+@pytest.mark.asyncio
+async def test_async_remove_snapshots_makes_api_call_for_stale_snapshots(
+    hass: HomeAssistant, aioclient_mock: AiohttpClientMocker
+):
+    backups = json.loads(load_fixture("backups.json"))["data"]["backups"][0:3]
+    backup_slugs = []
+    for backup in backups:
+        backup_slugs.append(backup["slug"])
+    await setup_supervisor_integration(aioclient_mock, backup_slugs)
+    thing = CleanUpSnapshots(hass, None)
+    await thing.async_remove_snapshots(backups)
+    # Assert we only call for the backups that are passed
+    assert aioclient_mock.call_count == len(backups)
+    # All the calls should be a delete verb.
+    for mock_call in aioclient_mock.mock_calls:
+        (method, _, _, _) = mock_call
+        assert method == "DELETE"
